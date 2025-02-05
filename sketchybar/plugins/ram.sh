51e20381ed14731850669a2e119bf1b395c56668
Gus Ryan <gus.ryan163@gmail.com>
2025-02-05 21:46:08 +0000

    add ram indicator

---
+++ sketchybar/plugins/ram.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+memory_stats=$(vm_stat)
+
+pages_active=$(echo "$memory_stats" | awk '/Pages active:/ {print $3}' | tr -d '.')
+pages_wired=$(echo "$memory_stats" | awk '/Pages wired down:/ {print $4}' | tr -d '.')
+pages_compressed=$(echo "$memory_stats" | awk '/Pages occupied by compressor:/ {print $5}' | tr -d '.')
+pages_inactive=$(echo "$memory_stats" | awk '/Pages inactive:/ {print $3}' | tr -d '.')
+
+total_used_pages=$((pages_active + pages_wired + pages_compressed + pages_inactive))
+
+memory_gb=$(echo "scale=2; $total_used_pages * $(pagesize) / 1024^3" | bc)
+
+sketchybar --set $NAME label="$memory_gb GB"

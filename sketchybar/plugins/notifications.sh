49a55043e5cc0ee600d5c2410e28113c08bb00af
Gus Ryan <gus.ryan163@gmail.com>
2025-02-10 22:49:27 +0000

    fix teams plugin

--- sketchybar/plugins/notifications.sh
+++ sketchybar/plugins/notifications.sh
@@ -1,34 +1,20 @@
 #!/bin/sh
 
-source "$CONFIG_DIR/colors.sh" # Loads all defined colors
-
-NOTIFICATION_STATUS_COLOR=$TRANSPARENT
-
-STATUS_LABEL=$(lsappinfo info -only StatusLabel "Microsoft Outlook")
-if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
-    LABEL="${BASH_REMATCH[1]}"
-
-    if [[ $LABEL == "•" ]]; then
-          NOTIFICATION_STATUS_COLOR=$TRANSPARENT_YELLOW
-    elif [[ $LABEL =~ ^[0-9]+$ ]]; then
-        NOTIFICATION_STATUS_COLOR=$TRANSPARENT_RED
-    fi
-fi
-
-sketchybar --set outlook label="${LABEL}"
-
-
 STATUS_LABEL=$(lsappinfo info -only StatusLabel "Microsoft Teams")
-if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
-    LABEL="${BASH_REMATCH[1]}"
-
-   if [[ $LABEL == "•" ]]; then
-        if [[ $NOTIFICATION_STATUS_COLOR != $TRANSPARENT_RED ]]; then
-          NOTIFICATION_STATUS_COLOR=$TRANSPARENT_YELLOW
-        fi
-    elif [[ $LABEL =~ ^[0-9]+$ ]]; then
-        NOTIFICATION_STATUS_COLOR=$TRANSPARENT_RED
-    fi
+if [[ $STATUS_LABEL =~ \"label\"=\"([0-9]+)\" ]]; then
+  LABEL="${BASH_REMATCH[1]}"
+  sketchybar --set teams  label="${LABEL}" \
+                          label.color="0xffffffff" \
+                          icon.color="0xffffffff" \
+                          background.color="0x33ed8796"
+elif [ "$STATUS_LABEL" ]; then
+  sketchybar --set teams  label="•" \
+                          label.color="0xffffffff" \
+                          icon.color="0xffffffff" \
+                          background.color="0x44001f30"
+else
+  sketchybar --set teams  label="•" \
+                          label.color="0x99ffffff" \
+                          icon.color="0x99ffffff" \
+                          background.color="0x22001f30"
 fi
-
-sketchybar --set teams label="${LABEL}"
